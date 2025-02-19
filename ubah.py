2ccdc36e9345472b6debed7eeec711dd72455f61
ainxpro6 <ainxpro6@gmail.com>
2025-02-19 10:28:29 +0700

    ubah.py

--- ubah.py
+++ ubah.py
@@ -27,7 +27,7 @@ def process_data(text):
     sku_pattern = r"SKU: ([^\n]+)"
     qty_pattern = r"Qty: (\d+)"  # Menambahkan pola untuk Qty jika tersedia
 
-    ignore_keywords = ["Jumlah produk", "Tanggal Cetak", "Dicetak Oleh", "Jumlah Pesanan", "Picking List", "PICK", "Bogor Loji", "Halaman"]
+    ignore_keywords = ["Jumlah produk", "TANJUNG PURA JAKARTA BARAT", "Tanggal Cetak", "Dicetak Oleh", "Jumlah Pesanan", "Picking List", "PICK", "Bogor Loji", "Halaman"]
 
     data = []
     current_product = {}
